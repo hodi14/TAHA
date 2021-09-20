208a29560b73bbd32a1c1b9f2d0c103c80ed210f
Hodi14 <alirezahodaee14@gmail.com>
2021-09-20 09:46:50 +0430

    even I don't know

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../public/assets/fonts/fontawesome-pro-5.15.1-web-ulabs/css/all.min.css'
 import '../node_modules/bootstrap-css-only/css/bootstrap.min.css'; 
 import '../node_modules/mdbreact/dist/css/mdb.css';
 import '../styles/css/styles.css';
-import Layout from '../Components/layout';
+import Layout from '../Components/Layout';
 
 export default function MyApp({ Component, pageProps }) {
   return (
