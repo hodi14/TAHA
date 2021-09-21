2aae68d39efcdccd0aa4a63d79172e5ea3ab3a69
Hodi14 <alirezahodaee14@gmail.com>
2021-09-21 13:11:26 +0430

    fixed file path

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../node_modules/bootstrap-css-only/css/bootstrap.min.css';
 import '../node_modules/mdbreact/dist/css/mdb.css';
 import '../styles/css/styles.css';
 import { useEffect, useState } from 'react';
-import Layout from '../Components/layout';
+import Layout from '../Components/Layout';
 
 export default function MyApp({ Component, pageProps }) {
   const [isLoaded, setLoad] = useState(false);
