113c9f7c19cefe6bf72f9c1529b2412abf7614bc
Hodi14 <alirezahodaee14@gmail.com>
2021-09-21 14:09:44 +0430

    test

---
+++ Data/ProjectsDetail.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: "SamCafe",
+    id: "01",
+    description:
+      "Rebranding & change the website for moreuser friendly and having better user interface",
+    technologies: "App that used : Figma, Adobe Ps, Adobe Ai",
+    imgPath: "samCafe.jpg",
+  },
+  {
+    name: "YasmineGold",
+    id: "02",
+    description:
+      "Rebranding & change the website for moreuser friendly and having better user interface",
+    technologies: "App that used : Figma, Adobe Ps, Adobe Ai",
+    imgPath: "yasmineGold.jpg",
+  },
+  {
+    name: "Faramouj",
+    id: "03",
+    description:
+      "Rebranding & change the website for moreuser friendly and having better user interface",
+    technologies: "App that used : Figma, Adobe Ps, Adobe Ai",
+    imgPath: "faramouj.jpg",
+  },
+  {
+    name: "HERA",
+    id: "04",
+    description:
+      "Rebranding & change the website for moreuser friendly and having better user interface",
+    technologies: "App that used : Figma, Adobe Ps, Adobe Ai",
+    imgPath: "hera.jpg",
+  },
+  {
+    name: "MAMMUT Containers",
+    id: "05",
+    description:
+      "Rebranding & change the website for moreuser friendly and having better user interface",
+    technologies: "App that used : Figma, Adobe Ps, Adobe Ai",
+    imgPath: "mammutContainers.jpg",
+  },
+];
